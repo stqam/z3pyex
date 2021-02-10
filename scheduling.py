427682fc29fef06be63d552aa2945e7c150552f8
Arie Gurfinkel <arie.gurfinkel@gmail.com>
2021-02-10 11:53:31 -0500

    updated scheduling to python3

--- scheduling.py
+++ scheduling.py
@@ -1,28 +1,29 @@
-from z3 import *
+import sys
+from z3 import And, Or, Solver, Ints
 
 
-print """
+print("""
 t11, t12, t21, t22, t31, t32 = Ints('t11 t12 t21 t22 t31 t32')
-"""
+""")
 
 sys.stdin.readline()
 
-print """
+print("""
 s = Solver()
 
 s.add(And([t11 >= 0, t12 >= t11 + 2, t12 + 1 <= 8]))
 s.add(And([t21 >= 0, t22 >= t21 + 3, t22 + 1 <= 8]))
-s.add(And([t31 >= 0, t32 >= t31 + 2, t32 + 3 <= 8]))"""
+s.add(And([t31 >= 0, t32 >= t31 + 2, t32 + 3 <= 8]))""")
 
 sys.stdin.readline()
 
-print """
+print("""
 s.add(Or(t11 >= t21 + 3, t21 >= t11 + 2))
 s.add(Or(t11 >= t31 + 2, t31 >= t11 + 2))
 s.add(Or(t21 >= t31 + 2, t31 >= t21 + 3))
 s.add(Or(t21 >= t22 + 1, t22 >= t12 + 1))
 s.add(Or(t12 >= t32 + 3, t32 >= t12 + 1))
-s.add(Or(t22 >= t32 + 3, t32 >= t22 + 1))"""
+s.add(Or(t22 >= t32 + 3, t32 >= t22 + 1))""")
 
 sys.stdin.readline()
 
@@ -42,18 +43,18 @@ s.add(Or(t21 >= t22 + 1, t22 >= t12 + 1))
 s.add(Or(t12 >= t32 + 3, t32 >= t12 + 1))
 s.add(Or(t22 >= t32 + 3, t32 >= t22 + 1))
 
-print """
+print("""
 print s.check()
-"""
+""")
 sys.stdin.readline()
 
-print ">>", s.check()
+print(">>", s.check())
 sys.stdin.readline()
 
-print """
-print s.model()"""
+print("""
+print s.model()""")
 sys.stdin.readline()
 
-print ">>", s.model()
+print(">>", s.model())
 
 sys.stdin.readline()
